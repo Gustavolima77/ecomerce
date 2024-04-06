aea2ab02e4da28b2580423a0934b98ce6aa98cda
gustavo pereira lima <gustavop.lima777@gmail.com>
2024-04-06 09:45:24 -0300

    feat: add mouses section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -21,6 +21,14 @@ export default async function Home() {
       },
     },
   });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
   return (
     <div className="flex flex-col gap-8">
       <PromoBanner
@@ -52,6 +60,11 @@ export default async function Home() {
           src="/Banner-Fones-(3).png"
           alt="até 55% de desconto em mouses!"
         />
+      </div>
+
+      <div>
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
