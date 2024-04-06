2ab080b1104352fa8b718d6ef30b0b89be48ccf8
gustavo pereira lima <gustavop.lima777@gmail.com>
2024-04-06 03:43:24 -0300

    feat: add second banner home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,13 +22,23 @@ export default async function Home() {
         sizes="100vw"
       />
 
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
       </div>
 
-      <div className="mt-8 px-5">
+      <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-Mouses-(02).png"
+        alt="até 55% de desconto em mouses!"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+      />
     </div>
   );
 }
