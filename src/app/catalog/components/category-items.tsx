9769aee63b5db7c5fcfed22d25a3ba8800f9c80f
gustavo pereira lima <gustavop.lima777@gmail.com>
2024-04-07 00:56:05 -0300

    feat: add page to show category product

--- src/app/catalog/components/category-items.tsx
+++ src/app/catalog/components/category-items.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -7,25 +8,28 @@ interface CategoryItemProps {
 
 const CategoryItems = ({ category }: CategoryItemProps) => {
   return (
-    <div className=" flex flex-col">
-      <div className="bg-category-item-gradient from-purple flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg ">
-        <Image
-          src={category.imageUrl}
-          alt={category.name}
-          width={0}
-          height={0}
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          sizes="100vw"
-          style={{
-            objectFit: "contain",
-          }}
-        />
-      </div>
+    <Link href={`/category/${category.slug}`}>
+      {" "}
+      <div className="flex flex-col">
+        <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient ">
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={0}
+            height={0}
+            className="h-auto max-h-[100%] w-auto max-w-[80%]"
+            sizes="100vw"
+            style={{
+              objectFit: "contain",
+            }}
+          />
+        </div>
 
-      <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
-        <p className="text-center text-sm font-semibold">{category.name}</p>
+        <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
+          <p className="text-center text-sm font-semibold">{category.name}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
