1f19f762ad71891db166921bc5eeaf15dae00c9b
gustavo pereira lima <gustavop.lima777@gmail.com>
2024-04-08 17:20:00 -0300

    feat: add Productimages components

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,24 +1,28 @@
 import { prismaClient } from "@/lib/prisma";
+import Productimages from "./components/product-images";
 
 interface ProductDetailsPageProps {
-    params: {
-        slug: string
-    }
+  params: {
+    slug: string;
+  };
 }
 
+const ProductDetailsPage = async ({
+  params: { slug },
+}: ProductDetailsPageProps) => {
+  const product = await prismaClient.product.findFirst({
+    where: {
+      slug: slug,
+    },
+  });
 
+  if (!product) return null;
 
+  return (
+    <h1 className="p-5">
+      <Productimages imageUrls={product.imageUrls} name={product.name} />
+    </h1>
+  );
+};
 
-const ProductDetailsPage = async ({params: {slug}}: ProductDetailsPageProps) => {
-    const product = await prismaClient.product.findFirst({
-        where: {
-            slug: slug,
-        }
-    })
-
-    if (!product) return null
-
-    return ( <h1>{product.name}</h1> );
-}
- 
-export default  ProductDetailsPage ;
+export default ProductDetailsPage;
